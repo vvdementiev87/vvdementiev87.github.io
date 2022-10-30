3491e68e2af0af7a73c061130e6a23a8e59ac108
vvdementiev87 <91119943+vvdementiev87@users.noreply.github.com>
2022-10-30 12:00:02 +0300

    Feat 1024 768

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
 import { Link } from "react-scroll";
 
 export const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpen = () => (isOpen ? setOpen(false) : setOpen(true));
   return (
-    <div id="home" className={styles.container + " " + styles.header}>
-      <img src={"./images/DEVAVI_logo_white.png"} alt="Logo" />
-      <ul className={styles.list}>
-        <li>
-          <Link activeClass="active" to="about" spy={true} smooth={true}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="projects" spy={true} smooth={true}>
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link to="skills" spy={true} smooth={true}>
-            Skills
-          </Link>
-        </li>
-        <li>
-          <Link to="exploring" spy={true} smooth={true}>
-            Studing
-          </Link>
-        </li>
-        <li>
-          <Link to="contacts" spy={true} smooth={true}>
-            Contacts
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div id="home" className={styles.container + " " + styles.header}>
+        <img
+          className={styles.imgLogo}
+          src={"./images/DEVAVI_logo_white.png"}
+          alt="Logo"
+        />
+        <img
+          className={styles.imgMenu}
+          src={"./images/barsSolid.svg"}
+          alt="Menu"
+          onClick={handleOpen}
+        />
+      </div>
+      {isOpen && (
+        <ul className={styles.container + " " + styles.list}>
+          <li>
+            <Link activeClass="active" to="about" spy={true} smooth={true}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="projects" spy={true} smooth={true}>
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" spy={true} smooth={true}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="exploring" spy={true} smooth={true}>
+              Studing
+            </Link>
+          </li>
+          <li>
+            <Link to="contacts" spy={true} smooth={true}>
+              Contacts
+            </Link>
+          </li>
+        </ul>
+      )}
+    </>
   );
 };
